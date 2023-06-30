84525e16d050e39cd8b8e564370edc8c21b8e327
sahishnu1123 <102507316+sahishnu1123@users.noreply.github.com>
2023-06-30 09:15:08 +0530

    Add files via upload

---
+++ delete.sql
@@ -0,0 +1,5 @@
+drop table users;
+drop table posts;
+drop table comments;
+drop table votes;
+drop table tags;
---
+++ populate.sql
@@ -0,0 +1,5 @@
+insert into tags (tag_name) values('Sport');
+insert into tags (tag_name) values('Doubt');
+insert into tags (tag_name) values('Education');
+insert into tags (tag_name) values('Entertainment');
+insert into tags (tag_name) values('Health');
